7b24b5bd5f74f77f6d7e33011d8fc0e3c3de8477
Mothupi Mike Ramogayana <mike.ramogayana@gmail.com>
2021-10-05 19:14:42 +0200

    Project Created

---
+++ Vendiza Api/Vendiza Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vendiza_Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
